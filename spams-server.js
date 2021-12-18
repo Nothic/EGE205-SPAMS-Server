5196a083473e57e7f68fbb4f63fa0635ffd36750
Nothic <noth.tanyh@gmail.com>
2021-12-18 11:34:37 +0800

    added beaglebone connection

--- spams-server.js
+++ spams-server.js
@@ -3,17 +3,37 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/spams-server.html");
 });
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
-    });
+var clients = {};
+
+io.on("connection", (socket) => {
+  console.log("new client connected");
+  clients[socket.id] = socket;
+  socket.on("ping servo", (servoState) => {
+    console.log("Servostate: " + servoState);
   });
+  socket.on("disconnect", () => {
+    console.log("a client disconnected");
+    delete clients[socket.id];
+  });
+  socket.on("BBBW1_Motion", () => {
+    console.log("bbbw1 picked up motion! om nom nom");
+  });
+  socket.on("BBBW1_NoMotion", () => {
+    console.log("bbbw1 picked up no motion! om nom nom");
+  });
+});
 
 server.listen(3001, () => {
   console.log("listening on *:3001");
